cfa0b0589ffa5bcc7d360fb36e537ee54e4b8173
Mark Chaffin <mark.chaffin@akimbosolutions.com>
2018-08-20 18:15:58 -0500

    initial build

---
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Jumbotron = () => (
+  <div className="jumbotron text-center bg-primary">
+    <h1>React Click</h1>
+    <p>
+      Click the images to Score, Beware not to Click Same Image Again!!!
+    </p>
+  </div>
+);
+
+export default Jumbotron;
